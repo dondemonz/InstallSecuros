ccadeded30f0a0f85a85a4e2f3fc54f36f0cebe5
root <kykareky05@mail.ru>
2021-06-16 10:49:34 +0300

    24

--- conftest.py
+++ conftest.py
@@ -1,13 +1,3 @@
-import pytest
-a = "test"
 
-
-
-@pytest.fixture
-def fix(request):
-    fixture = a
-    # функция disconnect передается в качестве параметра
-    request.addfinalizer(fixture.disconnect)
-    return fixture
 
 
